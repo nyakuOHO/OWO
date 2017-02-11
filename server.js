8b1db2baf02ee811c7b29a398b5760c2af2a489b
nyakuOHO <tea.zhen900708@gmail.com>
2017-02-11 18:22:00 +0800

    Delet note

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// var http = require('http');
-// var server = http.createServer(handleRequest);
-// var handleRequest = function(request, response){
-//     response.writeHead(200,{'Content-Type':'text/plain'});
-//     response.end("你好");
-// }
-// server.listen(8080,'127.0.0.1',function(){
-//     console.log('HTTP伺服器 http://127.0.0.1:8080/ 上運行');
-// });
-
 var http = require('http')
 var express = require('express');
 var app = express();
